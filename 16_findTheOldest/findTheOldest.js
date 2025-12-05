6047776dc39359523f0e600c9245f847d8d7a4ec
Bryan <bryanwagoki2016@gmail.com>
2025-12-05 16:05:46 +0300

    Complete the function to correctly perform the calculations as specified.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
 
-};
+const findTheOldest = function (arr) {
+
+    arr.forEach((item) => {
+
+        if (!item.yearOfDeath) {
+            item["yearOfDeath"] = new Date().getFullYear();
+            
+        }
+        
+      })
+     arr.sort((a, b) => {
+
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+
+        return ageA - ageB
+
+    })
+return arr.pop()
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
